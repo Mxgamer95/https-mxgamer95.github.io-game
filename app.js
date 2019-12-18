9399ec1d22067fe5631f48796f5b548fe42babf4
Mxgamer95 <nicolas.ivorra.ni@gmail.com>
2019-12-18 04:18:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var score, roundsScore, activePlayer, dice;
+
+scores = [0,0];
+roundsScore = 0;
+activePlayer = 0;
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    //numbre aleatoire
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //afficher le resultat
+    var diceDOM = document.querySelector('.dice');
+    document.querySelector('.dice').style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // Actualiser le score si le nombre tiré est different de 1
+    if (dice !== 1) {
+        roundsScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundsScore;
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundsScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+//dice = Math.floor(Math.random() * 6) + 1;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//var x = document.querySelector('#score-0').textContent;
